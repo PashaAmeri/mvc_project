628522c20cb2970d71d6f348296f245f87785f4e
PashaAmeri <a.pasha.ameri@gmail.com>
2022-05-09 17:44:24 +0430

    added siteController class to mange get requests

---
+++ app/controller/SiteController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App_hospital\app\controller;
+
+class SiteController
+{
+
+    public static function homeControl()
+    {
+
+        echo 'home page';
+    }
+
+    public static function dashbordControl()
+    {
+
+        echo 'dashbord page';
+    }
+}
